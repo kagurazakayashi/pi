31a25e80e175bb32b4d98f1ea980258848774dc2
神楽坂雅詩 <cyscxchope@gmail.com>
2021-08-08 14:03:24 +0800

    LCD单色背光屏+I2C控制器

---
+++ Screen/LCD1602IIC.py
@@ -0,0 +1,27 @@
+# GND <-> GND
+# VCC <-> 5V
+# SDA <-> SDA1
+# SCL <-> SCL1
+
+# sudo apt-get install i2c-tools
+# sudo apt-get install python-smbus
+# sudo i2cdetect -l
+# sudo i2cdetect -y 1
+
+# pip3 install logx
+
+import time
+import LCD1602 as LCD # wget https://raw.githubusercontent.com/dengzii/RespberryPi/master/LCD1602_IIC/LCD1602.py
+
+# 初始化 LCD 驱动库
+LCD.init_lcd()
+# 显示字符串（第几个字符，第几行，字符串）
+LCD.print_lcd(2, 0, 'LINE 1')
+LCD.print_lcd(3, 1, 'LINE 2')
+# 背光开关 0/1
+LCD.turn_light(1)
+# 显示时间
+while True:
+    now = time.strftime('%m/%d %H:%M:%S', time.localtime(time.time()))
+    LCD.print_lcd(1, 1, now)
+    time.sleep(0.5)
